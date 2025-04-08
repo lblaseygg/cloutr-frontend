9b2b7ca304a75112431733ecb91b40b95738c44b
Luis Feliciano <justblasey@gmail.com>
2025-04-08 14:20:25 -0400

    Fix Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full bg-white">
-      <body className={`${inter.className} h-full`}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={`${inter.className} h-full antialiased`}>
         <MainLayout>
           {children}
         </MainLayout>
